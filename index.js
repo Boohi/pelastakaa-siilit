d8faf999e6a7a388a8217cbf9025fa3cdea9db8b
Otto Mäenpää <otto.maenpaa@gmail.com>
2018-09-16 23:38:16 +0300

    added heroku support

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //Imports
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -9,6 +11,15 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+
+//Setup cookies and initialize passport
+app.use(cookieSession({
+    maxAge: 30*24*60*60*1000,
+    keys: [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Routes
 require("./routes/authRoutes")(app);
