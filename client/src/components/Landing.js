d455c362c56f4656eb89d4ee398cd559646567e7
Otto Mäenpää <otto.maenpaa@gmail.com>
2018-09-20 10:58:01 +0300

    further frontend tweaks, mainly navbar and auth

---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>
+                LSS.ai
+            </h1>
+                Boost your processes
+        </div>
+    )
+}
+
+
+export default Landing;
