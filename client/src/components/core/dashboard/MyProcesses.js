f65681e8233f62beb71bd1f5195eedbd8306431b
Lensku <henri.lencioni@aalto.fi>
2018-09-28 20:41:19 +0300

    git-test tried to use Materialize to create some cards and buttons in Home.js and MyProcesses.js

--- client/src/components/core/dashboard/MyProcesses.js
+++ client/src/components/core/dashboard/MyProcesses.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 
 class MyProcesses extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            visibility: false
+        };
+    }
+    handleClick() {
+        this.setState(prev => {
+            return {
+                visibility: !prev.visibility
+            };
+        });
+    }
     render() {
         return (
             <div>
                 MyProcesses
                 <div>MyProcesses</div>
+                <a className="btn-floating-red" onClick={this.handleClick}>
+                    {this.state.visibility ? 'Hide details' : 'Show details'}
+                </a>
+                {this.state.visibility && (
+                    <div class="col s12 m7">
+                        <div class="card horizontal">
+                            <div class="card-content">
+                                <p>
+                                    I am a very simple card. I am good at containing small bits of
+                                    information.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         );
     }
