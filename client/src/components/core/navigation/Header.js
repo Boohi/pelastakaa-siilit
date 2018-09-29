1b475898fbff4a1a274a0db3bc77c83a4332ad99
Joona17 <joona.soratie@aalto.fi>
2018-09-29 21:41:43 +0300

    minor changes

--- client/src/components/core/navigation/Header.js
+++ client/src/components/core/navigation/Header.js
@@ -38,7 +38,7 @@ class Header extends Component {
                         className="left brand-logo txt"
                         style={{ marginLeft: 200 }}
                     >
-                        Pelastakaa Siilit
+                        PENIS
                     </Link>
                     <ul className="right">{this.renderContent()}</ul>
                 </div>
